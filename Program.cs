b8eac620bb560f0530f22bb7bdd438f293f5561a
tmonji <tmonj1@gmail.com>
2021-11-24 23:15:57 +0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+
+namespace opencvdemo
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine(Environment.CurrentDirectory);
+      using (var capture = new VideoCapture(@"input.mov"))
+      {
+        var img = new Mat();
+        capture.Read(img);
+        BitmapConverter.ToBitmap(img).Save(@"converted.jpg");
+      }
+    }
+  }
+}
